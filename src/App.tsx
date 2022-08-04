760fa4d575df0e3af3d7162ca73750c385117721
Lucas Cordeiro <lucas.cordeiro@mobilesaude.com.br>
2022-08-04 17:49:36 -0300

    adicionado componente header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {GlobalStyle} from "./styles/global";
+import {Header} from "./components/Header";
 
 export function App() {
   return (
-    <div className="App">
-     <h1>Hello world</h1>
-        <GlobalStyle/>
-    </div>
+    <>
+     <Header/>
+     <GlobalStyle/>
+    </>
   );
 }
 
