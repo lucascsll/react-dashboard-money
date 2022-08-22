2f1ca78b3b6b67fc6e9516fef0f9d96f36d19542
Lucas Cordeiro <lucas.cordeiro@mobilesaude.com.br>
2022-08-21 22:00:03 -0300

    estilização do modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import {Container} from "./styles";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -7,10 +8,20 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal ({ isOpen , onRequestClose } : NewTransactionModalProps ) {
     return (
-        <Modal onRequestClose={onRequestClose} isOpen={isOpen}>
-            <h2>
-                Cadastrar transacao
-            </h2>
+        <Modal
+            onRequestClose={onRequestClose}
+            isOpen={isOpen}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content">
+           <Container>
+               <h2>
+                   Cadastrar transacao
+               </h2>
+               <input placeholder="Título"/>
+               <input type="number" placeholder="Valor"/>
+               <input  placeholder="Categoria"/>
+               <button type="submit" >Cadastrar</button>
+           </Container>
         </Modal>
     )
 }
